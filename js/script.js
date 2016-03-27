7b9371e5f7812bd2f048f3f8af99b97d24fef6cd
brayn003 <rudraprasaddas@outlook.com>
2016-03-27 10:17:33 +0530

    removed unecessary notes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 window.onload = function () {
-    // var mediaExpand = document.getElementById("media-expand-arrow");
-    // var mediaBar = document.getElementById("media-bar");
-    // var mediaCross = document.getElementById("media-bar-cross");
     var messageFeed = document.getElementById("message-feed");
-    
-    // console.log(mediaExpand + mediaBar);
-    
-    // function mediaOpen(){
-    //     mediaBar.className += " media-bar-open"
-    // };
-    
-    // mediaExpand.addEventListener("click", mediaOpen, false);
-    
-    // function mediaClose(){
-    //     mediaBar.className = "media-bar"
-    // };
-    
-    // mediaCross.addEventListener("click", mediaClose, false);
     
     function wScroll(){
         console.log(messageFeed.scrollTop);
